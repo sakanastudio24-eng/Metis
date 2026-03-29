1d3546a12c655ed7c353ba0282f83025f2369585
sakanastudio24-eng <sakanastudio24@gmail.com>
2026-03-29 13:54:47 -0700

    chore(cleanup): stop tracking generated test output

--- .tmp-tests/src/shared/types/runtime.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.DEFAULT_METIS_SESSION_UI_STATE = void 0;
-exports.DEFAULT_METIS_SESSION_UI_STATE = {
-    scanScope: "single",
-    panelMode: "mini",
-    plusAnswers: {},
-    isPlusRefinementOpen: false
-};
